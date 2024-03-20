62c66a990ec97ea5d6ae3b55321a76d361806cc8
c0lares <mcolaresc@gmail.com>
2024-03-20 20:21:03 -0300

    First Commit

---
+++ src/main/java/com/c0lares/band_organizer/BandOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.c0lares.band_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c0lares/band_organizer/BandOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c0lares.band_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
